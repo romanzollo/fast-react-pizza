368eda093e63a19e243a61ac545f178f490d749f
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-10-25 22:04:49 +0200

    building the AppLayout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+    return (
+        <header>
+            <Link to="/">Fast React Coffee Co.</Link>
+        </header>
+    );
+}
+
+export default Header;
