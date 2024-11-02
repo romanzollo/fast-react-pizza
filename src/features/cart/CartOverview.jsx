27cdaa10d37d9df923713236f8f3f7aa412123d9
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-11-02 21:26:46 +0200

    styling text and color

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,16 +1,16 @@
 import { Link } from 'react-router-dom';
 
 function CartOverview() {
-    return (
-        <div>
-            <p>
-                <span>23 pizzas</span>
-                <span>$23.45</span>
-            </p>
+  return (
+    <div className="bg-stone-800 uppercase text-stone-200">
+      <p className="font-semibold text-stone-300">
+        <span>23 pizzas</span>
+        <span>$23.45</span>
+      </p>
 
-            <Link to="/cart">Open cart &rarr;</Link>
-        </div>
-    );
+      <Link to="/cart">Open cart &rarr;</Link>
+    </div>
+  );
 }
 
 export default CartOverview;
---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="text-sm font-semibold">Roman</div>;
+}
+
+export default Username;
