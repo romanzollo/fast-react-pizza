2a91937c04528831ee415868a6046a10fb703700
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-11-02 21:51:33 +0200

    spacing, borders, and display (the box model)

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Roman</div>;
+  return <div className="hidden text-sm font-semibold">Roman</div>;
 }
 
 export default Username;
