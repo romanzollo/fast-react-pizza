acee7791f16734b803bb162188a549b8d7915950
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-11-03 20:44:36 +0200

    flexbox (tailwind)

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Roman</div>;
+  return <div className="hidden text-sm font-semibold md:block">Roman</div>;
 }
 
 export default Username;
