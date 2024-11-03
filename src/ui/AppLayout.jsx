22966d6cc5c5c98d10bed11f217bad93c409a72b
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-11-03 21:17:29 +0200

    CSS Grid (tailwind)

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,25 +5,27 @@ import CartOverview from '../features/cart/CartOverview';
 import Loader from './Loader';
 
 function AppLayout() {
-    // хук useNavigation (react-router v6.4) позволяет получить информацию о текущем состоянии навигации в приложении (loading, idle, submitting и т.д.) и AppLayout лучшее место для отображения индикатора загрузки так как индикатор загрузки теперь будет отображаться во всем приложении
-    const navigation = useNavigation();
+  // хук useNavigation (react-router v6.4) позволяет получить информацию о текущем состоянии навигации в приложении (loading, idle, submitting и т.д.) и AppLayout лучшее место для отображения индикатора загрузки так как индикатор загрузки теперь будет отображаться во всем приложении
+  const navigation = useNavigation();
 
-    // используем состояние глобальной навигации для отображения индикатора загрузки
-    const isLoading = navigation.state === 'loading';
+  // используем состояние глобальной навигации для отображения индикатора загрузки
+  const isLoading = navigation.state === 'loading';
 
-    return (
-        <div className="layout">
-            {isLoading && <Loader />}
+  return (
+    <div className="grid h-screen grid-rows-[auto_1fr_auto] ">
+      {isLoading && <Loader />}
 
-            <Header />
+      <Header />
 
-            <main>
-                <Outlet />
-            </main>
+      <div className="overflow-y-auto">
+        <main className=" mx-auto max-w-3xl  ">
+          <Outlet />
+        </main>
+      </div>
 
-            <CartOverview />
-        </div>
-    );
+      <CartOverview />
+    </div>
+  );
 }
 
 export default AppLayout;
