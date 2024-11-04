058c28015492318af0e58f74b74c28ba1425a43b
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-11-04 21:42:48 +0200

    configuring tailwind, custom font family, extend height

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    // extend - расширяет стандартные настройки
+    extend: {
+      fontFamily: {
+        // sans - для всей страницы
+        sans: 'Roboto Mono, monospace',
+      },
+
+      // переопределяем высоту вьюпорта (h-screen)
+      height: {
+        // Dynamic viewport height - '100vh' с учетом мобильных устройств
+        screen: '100dvh',
+      },
+
+      /* пример расширения цвета */
+      // colors: {
+      //   pizza: '#123456',
+      // },
+
+      /* пример расширения размера шрифта */
+      //   fontSize: {
+      //     huge: ['80rem', { lineHeight: '1' }],
+      //   },
+    },
   },
   plugins: [],
 };
